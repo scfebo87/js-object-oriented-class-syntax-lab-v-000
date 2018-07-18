a0383e13378fd18c3aeac3be346e975c6a6459c7
Sean Christopher Febo <scf@techie.com>
2018-07-18 02:01:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-class User {
-  constructor(name, email) {
+class BoardMember {
+  constructor(name, homeState, training) {
     this.name = name;
-    this.email = email;
-  }
- 
-  sayHello() {
-    console.log(`Hello, my name is ${this.name}`);
-  }
-}
- 
- 
-class Teacher extends User {
-  sayHello(){
-    super.sayHello()
-    console.log('hello')
+    this.homeState = homeState;
+    this.training = training;
+    this.veto = function() { 
+      return `No, I must disagree`;
+  };
+    this.approve = function() { 
+      return `You can do that!`;
+  };
+    this.doCharity = function() { 
+      return `I like to help people.`;
+  };
+    this.releasePressStatement = function() { 
+      return `You will see great things from Scuber.`;
+  };
+    this.sayHi = function() { 
+      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+  };
   }
 }
